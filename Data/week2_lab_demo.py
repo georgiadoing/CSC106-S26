f7832309114a01823e96545a973bd3e88240be32
georgiadoing <georgia.doing@jax.org>
2026-04-09 17:18:30 -0400

    Built site for gh-pages

---
+++ Data/week2_lab_demo.py
@@ -0,0 +1,25 @@
+#######################################
+# Georgia Doing doingg@union.edu
+# 4/9/26
+# In class examples of variables of
+# different types and type-casting
+#
+#######################################
+
+# assigning variables
+# <var_name> = <value>
+x_int = 2
+y_str = "2"
+
+# operators
+o1 = x_int + x_int
+o2 = y_str + y_str
+
+print('o1 is', o1)
+print('o2 is', o2)
+
+# type-casting
+o3 = x_int + int(y_str)
+print('o3 is', o3)
+
+in_num = float(input())
