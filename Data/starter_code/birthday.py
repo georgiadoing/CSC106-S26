9edf8a78f09ccf7368ea6eff244240bb026b5156
georgiadoing <georgia.doing@jax.org>
2026-04-12 17:32:49 -0400

    Built site for gh-pages

---
+++ Data/starter_code/birthday.py
@@ -0,0 +1,12 @@
+
+# Writes the text of the happy birthday song for someone
+def happy_birthday(name):
+
+
+
+### DO NOT DELETE THIS LINE: beg testing
+
+# Use this area if you want to add some function calls with different
+# parameter values to test your function definition. For example:
+
+happy_birthday("Ginny")
---
+++ Data/starter_code/cricket.py
@@ -0,0 +1,14 @@
+
+# Computes the temperature based on the number 
+# of times a cricket chirps in 15 seconds
+def cricket_thermometer(chirps_15):
+   
+    
+
+### DO NOT DELETE THIS LINE: beg testing
+
+# Use this area if you want to add some function calls with different
+# parameter values to test your function definition.
+
+chirps_15 = 20
+print(chirps_15, "chirps in 15 seconds ==>", cricket_thermometer(chirps_15))
