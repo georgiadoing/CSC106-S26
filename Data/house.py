1a11ef722c7503e0d6610faf325de8ae2fde68dc
georgiadoing <georgia.doing@jax.org>
2026-04-16 10:43:37 -0400

    Built site for gh-pages

---
+++ Data/house.py
@@ -0,0 +1,26 @@
+# Starter code
+#
+# Draw the "Haus vom Nikolaus".
+
+import turtle
+import math
+
+turtle.pensize(3)
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(30)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(math.sqrt(100*100+100*100))
+turtle.right(135)
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(math.sqrt(100*100+100*100))
+turtle.left(45)
+
