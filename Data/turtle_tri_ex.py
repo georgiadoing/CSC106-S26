7b5aa370be7d4ac0651abfa6ae48b2bbebeeefce
georgiadoing <georgia.doing@jax.org>
2026-04-22 15:23:38 -0400

    Built site for gh-pages

---
+++ Data/turtle_tri_ex.py
@@ -0,0 +1,30 @@
+###############################################################################
+# Georgia Doing (doingg@union.edu)
+#
+# Turtle triangle with optional param
+# In class demo
+###############################################################################
+
+import turtle
+
+# example function with optional param
+def turt_triangle(color = "black"):
+    turtle.pencolor(color)
+    for side in range(3):
+        turtle.forward(75)
+        turtle.left(120)
+
+
+# set pensize so easier to see
+turtle.pensize(5)
+
+# draw a balck triangle (default color)
+turt_triangle()
+
+# move so cshapes don't overlap
+turtle.teleport(100,100)
+
+# draw a red triangle
+turt_triangle("red")
+
+
