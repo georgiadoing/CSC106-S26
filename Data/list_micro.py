907fdbb4c356191673b74fed4617d180b97dea4b
georgiadoing <georgia.doing@jax.org>
2026-05-11 17:51:36 -0400

    Built site for gh-pages

---
+++ Data/list_micro.py
@@ -0,0 +1,99 @@
+# Add an appropriate header up here.
+
+
+
+#############################################################
+# Write a statement that assigns a list of 5 integers (any
+# values) to the variable some_numbers.
+#############################################################
+
+# Question a): Replace this comment with your code
+
+
+
+print("A list of 5 integers:", some_numbers)
+
+
+
+######################################################################
+# Write a statement that concatenates the lists list1 and list2 and
+# assigns the result to the variable list1and2. (Note that the
+# starter file assigns some values to list1 and list2 so that you
+# can test your code. However, your statement should work for any
+# values of list1 and list2.)
+######################################################################
+
+
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6, 7]
+
+
+
+# Question b): Replace this comment with your code
+
+
+
+print("Concatenating the lists", list1, "and", list2, ":", list1and2)
+
+
+
+######################################################################
+# Write a statement that computes the sum of the lengths of list3 and
+# list4 and assigns the result to the variable combined_lengths.
+######################################################################
+
+
+
+list3 = [1, 2, 3, 4]
+list4 = [5, 6]
+
+
+
+# Question c): Replace this comment with your code
+
+
+
+print("Combined length of", list3, "and", list4, ":", combined_lengths)
+
+
+
+######################################################################
+# Write a statement that assigns the third element of list list5 to
+# the variable third_element.
+######################################################################
+
+
+
+list5 = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+
+
+# Question d): Replace this comment with your code
+
+
+
+print("The 3rd element of", list5, ":", third_element)
+
+
+
+######################################################################
+# Assume that list6 is a list of numbers. Write a statement that adds
+# the first and last element of list list6 and assigns the result to
+# the variable first_plus_last. (Note: make sure that your statement
+# works no matter how long list6 is.)
+######################################################################
+
+
+
+list6 = [1, 2, 3, 4, 5, 6, 7]
+
+
+
+# Question e): Replace this comment with your code
+
+
+
+print("Sum of the 1st and last element of", list6, ":", first_plus_last)
+
+
---
+++ Data/minimum.py
@@ -0,0 +1,28 @@
+# Replace this comment with an appropriate header up here.
+
+# Replace this comment with your function definition(s)
+
+
+# testing find_min
+l = [4, 7, 2, 5, 8, 5, 4]
+print("smallest of", l, ":", find_min(l))
+
+l = [-4, 5, 2, -5, 8, -5, 4]
+print("smallest of", l, ":", find_min(l))
+
+l = [56]
+print("smallest of", l, ":", find_min(l))
+
+l = []
+print("smallest of", l, ":", find_min(l))
+
+# testing drop_smallest
+#l = [-4, 5, 2, -5, 8, -5, 4]
+#print("dropping smallest from", l, ":", drop_smallest(l))
+
+#l = [0, 5, 2, 5, 8, 5, 4]
+#print("dropping smallest from", l, ":", drop_smallest(l))
+
+#l = [3, 3, 3, 3,]
+#print("dropping smallest from", l, ":", drop_smallest(l))
+
