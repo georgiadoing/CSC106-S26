fb49d8a842a1cf892bcdd0d40536a6bdd6f1e8f8
georgiadoing <georgia.doing@jax.org>
2026-05-17 23:30:08 -0400

    Built site for gh-pages

---
+++ Data/author_identification.py
@@ -0,0 +1,53 @@
+# Replace this comment with an appropriate header.
+
+# Replace this comment with your function definitions.
+
+
+
+### DO NOT DELETE THIS LINE: beg testing
+
+# Here is some code to help you test your implementation.
+
+#### Uncomment this section for testing make_word_frequency_dictionary
+
+# print("Testing make_word_frequency_dictionary")
+# bush = "state-union-gwbush-minus-2006.txt"
+# obama = "state-union-obama-minus-2014.txt"
+# bush_freq_d = make_word_frequency_dictionary(bush)
+# obama_freq_d = make_word_frequency_dictionary(obama)
+
+# word = "america"
+# print("Bush:", word, "-->", bush_freq_d[word])
+# print("Obama:", word, "-->", obama_freq_d[word])
+
+# word = "economy"
+# print("Bush:", word, "-->", bush_freq_d[word])
+# print("Obama:", word, "-->", obama_freq_d[word])
+
+# word = "war"
+# print("Bush:", word, "-->", bush_freq_d[word])
+# print("Obama:", word, "-->", obama_freq_d[word])
+
+
+#### Uncomment this section for testing make_word_probability_dictionary 
+
+# print("\nTesting make_word_probability_dictionary")
+# bush = "state-union-gwbush-minus-2006.txt"
+# obama = "state-union-obama-minus-2014.txt"
+# bush_freq_d = make_word_frequency_dictionary(bush)
+# obama_freq_d = make_word_frequency_dictionary(obama)
+# bush_prob_d = make_word_probability_dictionary(bush_freq_d)
+# obama_prob_d = make_word_probability_dictionary(obama_freq_d)
+
+# word = "america"
+# print("Bush:", word, "-->", bush_freq_d[word], "-->", bush_prob_d[word])
+# print("Obama:", word, "-->", obama_freq_d[word], "-->", obama_prob_d[word])
+
+# word = "economy"
+# print("Bush:", word, "-->", bush_freq_d[word], "-->", bush_prob_d[word])
+# print("Obama:", word, "-->", obama_freq_d[word], "-->", obama_prob_d[word])
+
+# word = "war"
+# print("Bush:", word, "-->", bush_freq_d[word], "-->", bush_prob_d[word])
+# print("Obama:", word, "-->", obama_freq_d[word], "-->", obama_prob_d[word])
+
